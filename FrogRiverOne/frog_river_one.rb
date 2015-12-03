2f397f15266c2f1cb292787b29714c905f00dcf4
piotrzientara <piotr.zientara@rebased.pl>
2015-12-03 19:51:30 +0100

    performance improved to 40%, O(N**2)

--- FrogRiverOne/frog_river_one.rb
+++ FrogRiverOne/frog_river_one.rb
@@ -3,12 +3,11 @@ class FrogRiverOne
   def solution(x, a)
     seconds = 0
     needed_leaves = Array(1..x)
-    needed_leaves.delete(a.shift)
-    until needed_leaves.empty? || a.empty? do
-      needed_leaves.delete(a.shift)
-      seconds += 1
+    needed_leaves.each do |el|
+      index = a.find_index(el)
+      return -1 if index == nil
+      seconds = [a.find_index(el), seconds].max
     end
-    return -1 unless needed_leaves.empty?
     seconds
   end
 
