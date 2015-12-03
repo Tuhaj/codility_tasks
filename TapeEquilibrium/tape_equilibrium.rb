d9b7e7511d1772258d34ba3ecef430d3dd688eaa
piotrzientara <piotr.zientara@rebased.pl>
2015-12-03 15:40:31 +0100

    changes approach, improves result from 91% to 100%

--- TapeEquilibrium/tape_equilibrium.rb
+++ TapeEquilibrium/tape_equilibrium.rb
@@ -1,13 +1,11 @@
 class TapeEquilibrium
 
   def solution(a)
-    split_points = ( a.size - 1)
-    left_sum = a[0]
-    right_array = a[1..-1]
+    left_sum = a.shift
+    right_array = a
     right_sum = right_array.reduce(:+)
     min = (left_sum - right_sum).abs
-    return min if split_points < 2
-    split_points.times do
+    until right_array.size == 1 do
       changing_element = right_array.shift
       left_sum += changing_element
       right_sum -= changing_element
--- TapeEquilibrium/tape_equilibrium_test.rb
+++ TapeEquilibrium/tape_equilibrium_test.rb
@@ -13,11 +13,16 @@ describe TapeEquilibrium do
 
   it "should return smallest difference between parts with big negative numbers" do
     array = [100, 1, 1, 1, -10000]
-    expect(TapeEquilibrium.new.solution(array)).to eql(9897)
+    expect(TapeEquilibrium.new.solution(array)).to eql(10097)
   end
 
   it "should return correct answer for double" do
     array = [-1000, 1000]
     expect(TapeEquilibrium.new.solution(array)).to eql(2000)
+  end
+
+  it "should return correct answer for small array" do
+    array =  [-10, -20, -30, -40, 100]
+    expect(TapeEquilibrium.new.solution(array)).to eql(20)
   end
 end
