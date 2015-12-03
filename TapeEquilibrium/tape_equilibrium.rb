3964c0e88b7a0c95014afa7f79156ed8fa95afe5
piotrzientara <piotr.zientara@rebased.pl>
2015-12-03 15:27:56 +0100

    fix for one test

--- TapeEquilibrium/tape_equilibrium.rb
+++ TapeEquilibrium/tape_equilibrium.rb
@@ -1,15 +1,16 @@
 class TapeEquilibrium
 
   def solution(a)
-    split_points = ( a.size - 1 )
-    right_array = a[1..-1]
+    split_points = ( a.size - 1)
     left_sum = a[0]
+    right_array = a[1..-1]
     right_sum = right_array.reduce(:+)
     min = (left_sum - right_sum).abs
+    return min if split_points < 2
     split_points.times do
       changing_element = right_array.shift
-      left_sum = left_sum + changing_element
-      right_sum = right_sum - changing_element
+      left_sum += changing_element
+      right_sum -= changing_element
       difference = (left_sum - right_sum).abs
       min = [min, difference].min
     end
--- TapeEquilibrium/tape_equilibrium_test.rb
+++ TapeEquilibrium/tape_equilibrium_test.rb
@@ -2,17 +2,22 @@ require_relative 'tape_equilibrium'
 
 describe TapeEquilibrium do
   it "should return smallest difference between parts" do
-    TEST_ARRAY = [3, 1, 2, 4, 3]
-    expect(TapeEquilibrium.new.solution(TEST_ARRAY)).to eql(1)
+    array = [3, 1, 2, 4, 3]
+    expect(TapeEquilibrium.new.solution(array)).to eql(1)
   end
 
   it "should return smallest difference between parts with negative numbers" do
-    TEST_ARRAY = [4, -5, 20, 1, 0]
-    expect(TapeEquilibrium.new.solution(TEST_ARRAY)).to eql(12)
+    array = [4, -5, 20, 1, 0]
+    expect(TapeEquilibrium.new.solution(array)).to eql(12)
   end
 
   it "should return smallest difference between parts with big negative numbers" do
-    TEST_ARRAY = [100, 1, 1, 1, -10000]
-    expect(TapeEquilibrium.new.solution(TEST_ARRAY)).to eql(9897)
+    array = [100, 1, 1, 1, -10000]
+    expect(TapeEquilibrium.new.solution(array)).to eql(9897)
+  end
+
+  it "should return correct answer for double" do
+    array = [-1000, 1000]
+    expect(TapeEquilibrium.new.solution(array)).to eql(2000)
   end
 end
