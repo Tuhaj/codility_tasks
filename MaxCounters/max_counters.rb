77ad4b138d3a9f2206fe475b3bad4b5a46241ea6
piotrzientara <piotr.zientara@rebased.pl>
2015-12-11 17:25:56 +0100

    tries to improve performance keeping maximum value in memory performance 80%

--- MaxCounters/max_counters.rb
+++ MaxCounters/max_counters.rb
@@ -1,11 +1,14 @@
 class MaxCounters
   def solution(number, array)
     counters = [0] * number
+    max = 0
     array.each do |e|
       if e <= number
-        counters[e - 1] += 1
+        current_value = counters[e - 1]
+        new_value = current_value + 1
+        max = new_value if max < new_value
+        counters[e - 1] = new_value
       else
-        max = counters.max
         counters = [max]  * number
       end
     end
