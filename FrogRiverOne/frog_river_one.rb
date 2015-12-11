1828d3585867bca4bc45a9e11abaa715d238f99b
piotrzientara <piotr.zientara@rebased.pl>
2015-12-11 11:28:27 +0100

    uses counting elements method from lesson

--- FrogRiverOne/frog_river_one.rb
+++ FrogRiverOne/frog_river_one.rb
@@ -1,14 +1,29 @@
 class FrogRiverOne
 
-  def solution(x, a)
-    seconds = 0
-    needed_leaves = Array(1..x)
-    needed_leaves.each do |el|
-      index = a.find_index(el)
-      return -1 if index == nil
-      seconds = [a.find_index(el), seconds].max
+# https://codility.com/media/train/2-CountingElements.pdf
+  def counting_elements(array)
+    # generates meta-array
+    # index - value of the array
+    # value - times the value occures
+    n = array.size
+    m = array.max
+    count = [0] * (m + 1)
+    Array(0..n-1).each do |index|
+      count[array[index]] += 1
     end
-    seconds
+    count
   end
 
+  def solution(x, array)
+    seconds = 0
+    needed_leaves = [0] * ( x + 1 )
+    array.each_with_index do |leaf, time|
+      if needed_leaves[leaf] == 0
+        needed_leaves[leaf] = 1
+        seconds += 1
+        return time if seconds == x
+      end
+    end
+    -1
+  end
 end
--- FrogRiverOne/frog_river_one_test.rb
+++ FrogRiverOne/frog_river_one_test.rb
@@ -15,4 +15,10 @@ describe FrogRiverOne do
     array = [1,3]
     expect(subject.solution(3, array)).to eql(-1)
   end
+
+  it "should change the way of storing the array" do
+    array = [0, 0, 4, 2, 4, 5]
+    expected_array = [2, 0, 1, 0, 2, 1]
+    expect(subject.counting_elements(array)).to eql(expected_array)
+  end
 end
