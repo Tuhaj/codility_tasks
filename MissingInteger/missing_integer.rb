89513b3bb64fe84421b5586195ae870d2c4f4a2f
piotrzientara <piotr.zientara@rebased.pl>
2015-12-11 15:26:25 +0100

    tries without creating any array, 100%

--- MissingInteger/missing_integer.rb
+++ MissingInteger/missing_integer.rb
@@ -1,29 +1,19 @@
 # https://codility.com/programmers/task/missing_integer/
 class MissingInteger
-
-  def counting_elements(array)
-    # generates meta-array
-    # index - value of the array
-    # value - times the value occures
-    n = array.size
-    max = array.max
-    min = array.min
-    count = [0] * (max + 1)
-    Array(0..n-1).each do |index|
-      if array[index] >= 0
-        count[array[index]] += 1
-      end
-    end
-    count
-  end
-
   def solution(array)
-    counted_elements = counting_elements(array)
-    return 1 if counted_elements.empty?
-    counted_elements.each_with_index do |element, index|
-      next if index == 0 # removes counters for 0
-      return index if element == 0
+    last_element = nil
+    missing_element = 1
+    array.sort!
+    max = array.last
+    array.each do |element, index|
+      next if element <= 0 || last_element == element
+      if element > missing_element
+        return missing_element
+      else
+        missing_element += 1
+      end
+      last_element = element
     end
-    array.max + 1
+    missing_element
   end
 end
