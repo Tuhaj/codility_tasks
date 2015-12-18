b0037c8ec6106a072b52847a1c4a8452aadc70d7
piotrzientara <piotr.zientara@rebased.pl>
2015-12-18 14:06:25 +0100

    failing test for last solution

--- MinAvgTwoSlice/min_avg_two_slice_test.rb
+++ MinAvgTwoSlice/min_avg_two_slice_test.rb
@@ -5,4 +5,9 @@ describe MinAvgTwoSlice do
     array = [4, 2, 2, 5, 1, 5, 8]
     expect(subject.solution(array)).to eql(1)
   end
+
+  it "should return the begining of min avg slice for small array" do
+    array = [5, 6, 3, 4, 9]
+    expect(subject.solution(array)).to eql(2)
+  end
 end
