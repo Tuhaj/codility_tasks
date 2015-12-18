d12cb2b9b59c52792ae64d036476874dc71cba1c
piotrzientara <piotr.zientara@rebased.pl>
2015-12-18 14:55:28 +0100

    different try, 60% - 80% correctness 40% performance

--- MinAvgTwoSlice/min_avg_two_slice.rb
+++ MinAvgTwoSlice/min_avg_two_slice.rb
@@ -1,42 +1,28 @@
 class MinAvgTwoSlice
-
-  def prefix_sums(array)
-    size = array.size
-    prefixes = Array.new(size, 0)
-    array.each_with_index do |element, index|
-      prefixes[index] = prefixes[index - 1] + element
-    end
-    prefixes
-  end
-
+  # this solution is based on the observation that if we have four elements with
+  # with smaller average than some two elements, there is a pair that has a
+  # smaller average
   def solution(array)
     size = array.size
     prefixes = Array.new(size, 0)
-    min_avg_slice_start = 0
     prefixes[0] = array[0]
-    slice = [array[0]]
-    slice_members = 1
     min_avg = Float::INFINITY
+    last_trio_avg = Float::INFINITY
+    min_avg_slice_start = 0
     array.each_with_index do |element, index|
       next if index == 0
-      slice << element
-      sum = prefixes[index - 1] + element
-      slice_members += 1
-      avg = sum.to_f / slice_members
-      if avg < min_avg
-        while slice_members > 2
-          slice_members -= 1
-          first_element = slice.shift
-          test_sum = sum - first_element
-          avg = test_sum.to_f / slice_members
-          min_avg = avg if avg < min_avg
+      prefixes[index] = prefixes[index - 1] + element
+      if index > 1
+        last_pair_avg = (prefixes[index] - prefixes[index - 2]).to_f / 2
+        last_trio_avg = (prefixes[index] - prefixes[index - 3]).to_f / 3 if index > 3
+        min_avg = [last_pair_avg, last_trio_avg, min_avg].min
+        if last_pair_avg == min_avg
+          min_avg_slice_start = index - 1
+        elsif last_trio_avg == min_avg
+          min_avg_slice_start = index - 2
         end
-        min_avg = avg
-        min_avg_slice_start = index + 1 - slice_members
-        prefixes[index] = sum
       else
-        slice_members = 1
-        prefixes[index] = element
+        min_avg = prefixes[index].to_f / 2
       end
     end
     min_avg_slice_start
