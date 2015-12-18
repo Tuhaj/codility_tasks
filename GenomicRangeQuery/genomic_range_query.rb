0e799e3b5cd33f10ce5e1a61862a20bbb39844cb
piotrzientara <piotr.zientara@rebased.pl>
2015-12-18 17:15:02 +0100

    improves performance to 100%, score: 100%

--- GenomicRangeQuery/genomic_range_query.rb
+++ GenomicRangeQuery/genomic_range_query.rb
@@ -1,26 +1,22 @@
 class GenomicRangeQuery
 
-  IMPACT_TABLE = {
-    "A" => 1,
-    "C" => 2,
-    "G" => 3,
-    "T" => 4
-  }
-
-  def minimal_value(string)
-    min = 4
-    string.split('').each do |el|
-      min = IMPACT_TABLE[el] if min > IMPACT_TABLE[el]
-      return min if min == 1
+  def return_min_in_substring(string)
+    if string.include?("A")
+      return 1
+    elsif string.include?("C")
+      return 2
+    elsif string.include?("G")
+      return 3
+    else
+      return 4
     end
-    min
   end
 
   def solution(s, p, q)
     answers = []
     p.each_with_index do |e, index|
       substring = s[e..q[index]]
-      answers << minimal_value(substring)
+      answers << return_min_in_substring(substring)
     end
     answers
   end
--- GenomicRangeQuery/genomic_range_query_test.rb
+++ GenomicRangeQuery/genomic_range_query_test.rb
@@ -8,19 +8,4 @@ describe GenomicRangeQuery do
     q = [4, 5, 6]
     expect(subject.solution(s, p, q)).to eql( [2, 4, 1])
   end
-
-  it "should return minimal value" do
-    s = "GGG"
-    expect(subject.minimal_value(s)).to eql(3)
-  end
-
-  it "should return minimal value with A" do
-    s = "GGAG"
-    expect(subject.minimal_value(s)).to eql(1)
-  end
-
-  it "should return minimal value with A" do
-    s = "GGGGGTTTTTTCG"
-    expect(subject.minimal_value(s)).to eql(2)
-  end
 end
