0990c8fd6fa636dcb221e8d97fd8a88c7122002b
piotrzientara <piotr.zientara@rebased.pl>
2016-01-22 15:16:25 +0100

    solves conflict with opening and closing in the same point

--- NumberOfDiscIntersections/number_of_disc_intersections.rb
+++ NumberOfDiscIntersections/number_of_disc_intersections.rb
@@ -12,55 +12,31 @@ class NumberOfDiscIntersections
     end
     openings.sort!
     closings.sort!
-    puts "#{openings.inspect}"
-    puts "#{closings.inspect}"
-    first_element = openings.first
+    minimum = openings.first
+    minimum = 0 if minimum > 0
     last_element = closings.last
-    size = (first_element - last_element).abs + 1
-    flat_array = Array.new(size, 0)
+    size = (minimum - last_element).abs + 1
+    flat_array = Array.new(size*2, 0)
     openings.each do |opening|
-      flat_array[opening - first_element] += 1
+      even_index = 2*(opening - minimum)
+      flat_array[even_index] += 1
     end
     closings.each do |closing|
-      flat_array[closing - first_element] -= 1
+      odd_index = 2*(closing - minimum)+1
+      flat_array[odd_index] -= 1
     end
     open = 0
     flat_array.each do |el|
       if el > 0
         open += el
       elsif el < 0
-        intersections += open
-        open += el
+        (-el).times do
+          intersections += (open -1) if open > 1
+          open -= 1
+        end
       end
     end
     intersections
   end
 end
 
-# sorting array of arrays sorts by first element:
-# a = [[1,5], [-1,3], [0, 1], [2,4]]
-# a.sort! = [[-1, 3], [0, 1], [1, 5], [2, 4]]
-
-
-
-
-    # count = 0
-    # index_of_zero = 0
-    # point_owners = Array.new(array.size, 0)
-    # array.each_with_index do |radius, index|
-    #   open_point = index_of_zero + index - radius
-    #   close_point = index_of_zero + index + radius
-    #   if open_point < 0
-    #     last_index_of_zero = index_of_zero
-    #     index_of_zero = -open_point if open_point < last_index_of_zero
-    #     prefix = Array.new((index_of_zero - last_index_of_zero), 0)
-    #     point_owners = prefix + point_owners unless prefix.empty?
-    #   end
-    #   Array(open_point..close_point).each do |index|
-    #     point_owners[index] ||= 0
-    #     point_owners[index] = point_owners[index] + 1
-    #   end
-    #   max = point_owners[open_point..close_point].max
-    #   count += max
-    # end
-    # count
