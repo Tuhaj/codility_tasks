8741bfdce0925dae79b8c64d27e3dacccbc960f5
piotrzientara <piotr.zientara@rebased.pl>
2016-01-22 17:19:31 +0100

    brackets - first try 100% but code poor

--- Brackets/brackets.rb
+++ Brackets/brackets.rb
@@ -1,5 +1,34 @@
 class Brackets
   def solution(string)
-    1
+    return 1 if string == ""
+    matching_brackets = {
+      ")" => "(",
+      "}" => "{",
+      "]" => "["
+    }
+    bracket_counters = {
+      "(" => 0,
+      "{" => 0,
+      "[" => 0,
+    }
+    opening_brackets = ["(", "[", "{"]
+    closing_brackets = [")", "]", "}"]
+    array = string.split('')
+    last_bracket = array.shift
+    return 0 if closing_brackets.include?(last_bracket)
+    bracket_counters[last_bracket] += 1
+    array.each do |bracket|
+      return 0 if closing_brackets.include?(bracket) && opening_brackets.include?(last_bracket) && matching_brackets[bracket] != last_bracket
+      if opening_brackets.include?(bracket)
+        bracket_counters[bracket] += 1
+      else
+        return 0 if bracket_counters[matching_brackets[bracket]] == 0
+        bracket_counters[matching_brackets[bracket]] -= 1
+      end
+      last_bracket = bracket
+    end
+    return 0 if bracket_counters['('] != 0 || bracket_counters['['] != 0 ||
+      bracket_counters['{'] != 0
+    return 1
   end
 end
--- Brackets/brackets_test.rb
+++ Brackets/brackets_test.rb
@@ -5,4 +5,29 @@ describe Brackets do
     string = "{[()()]}"
     expect(subject.solution(string)).to eql 1
   end
+
+  it 'should return 1 for basic string example with one bracket' do
+    string = "{}"
+    expect(subject.solution(string)).to eql 1
+  end
+
+  it 'should return 0 for basic string example with broken bracket' do
+    string = "{"
+    expect(subject.solution(string)).to eql 0
+  end
+
+  it 'should return 0 for changing brackets' do
+    string = "({)}"
+    expect(subject.solution(string)).to eql 0
+  end
+
+  it 'should return 0 for obverse brackets' do
+    string = ")("
+    expect(subject.solution(string)).to eql 0
+  end
+
+  it 'should return 1 for no brackets' do
+    string = ""
+    expect(subject.solution(string)).to eql 1
+  end
 end
