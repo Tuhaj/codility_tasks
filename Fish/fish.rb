1f9bb79b5eccded95086a6d0fe2796d02b7e22a3
piotrzientara <piotr.zientara@rebased.pl>
2016-01-28 11:23:05 +0100

    refactoring fish

--- Fish/fish.rb
+++ Fish/fish.rb
@@ -3,21 +3,23 @@ class Fish
     fish_going_down = []
     alive = 0
     a.each_with_index do |fish, index|
-      if b[index] == 1 # fish going downstream
-        fish_going_down << fish
-      else # fish going upstream
-        if fish_going_down.empty?
-          alive += 1
-          next
-        else
-          while fish_going_down.size > 0 && fish_going_down.last < fish
-            fish_going_down.pop
-          end
-        end
-        alive += 1 if fish_going_down.empty?
-      end
+      # fish going downstream
+      ( fish_going_down << fish; next ) if b[index] == 1
+      # fish going upstream
+      ( alive += 1; next ) if fish_going_down.empty?
+      removes_all_smaller_form_tail(fish_going_down, fish)
+      alive += 1 if fish_going_down.empty? # the upstream fish which ate all
     end
     alive += fish_going_down.count
   end
 
+  def removes_all_smaller_form_tail(array, limit)
+    # array.reverse.drop_while{ |n| n < limit }.reverse # this doesn't work!
+    while array.size > 0 && array.last < limit
+      array.pop
+    end
+    array
+  end
+
 end
+
--- Fish/fish_test.rb
+++ Fish/fish_test.rb
@@ -6,4 +6,11 @@ describe Fish do
     b = [0, 1, 0, 0, 0]
     expect(subject.solution(a,b)).to eql(2)
   end
+
+  describe '#removes_all_smaller_form_tail' do
+    it 'should remove all elements smaller from top of the array' do
+      array = [1,4,8,6,2,0]
+      expect(subject.removes_all_smaller_form_tail(array, 7)).to eql([1,4,8])
+    end
+  end
 end
