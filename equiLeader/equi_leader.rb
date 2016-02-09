59efea1e8bca738c9b5d4b5aab2439fd41ee8003
piotrzientara <piotr.zientara@rebased.pl>
2016-02-09 16:36:12 +0100

    equi leaders, correct solution without performance 50%

--- equiLeader/equi_leader.rb
+++ equiLeader/equi_leader.rb
@@ -1,5 +1,38 @@
 class EquiLeader
   def solution(array)
+    equi_leaders = []
+    array.size.times do |index|
+      first_leader = find_leader((array[0..index]))
+      if first_leader != -1
+        second_part = array[(index+1)..-1]
+        second_leader = find_leader(second_part)
+        if second_leader == first_leader
+          equi_leaders << index
+        end
+      end
+    end
+    equi_leaders.size
+  end
 
+  def find_leader(array)
+    size = 0
+    value = array.first
+    array.each do |el|
+      if (size == 0)
+        size += 1
+        value = el
+      else
+        value != el ? size -= 1 : size += 1
+      end
+    end
+    candidate = -1
+    candidate = value if (size > 0)
+    leader = -1
+    count = 0
+    array.each do |el|
+      count += 1 if (el == candidate)
+    end
+    leader = candidate if (count > array.size / 2)
+    leader
   end
 end
--- equiLeader/equi_leader_test.rb
+++ equiLeader/equi_leader_test.rb
@@ -2,7 +2,7 @@ require_relative 'equi_leader'
 
 describe EquiLeader do
   it 'should find equi leader' do
-    array = [4, 3, 4, 4, 4, 2, 3, 3]
+    array = [4, 3, 4, 4, 4, 2]
     expect(subject.solution(array)).to eql(2)
   end
 end
