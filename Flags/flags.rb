0e2bf559db21f1e6c9903442bec1b42e5df98e4a
piotrzientara <piotr.zientara@rebased.pl>
2016-03-22 22:16:10 +0100

    correctness 62%

--- Flags/flags.rb
+++ Flags/flags.rb
@@ -1,12 +1,13 @@
+require 'byebug'
 class Flags
-
+  #there are too many flags in this solution
   def solution(array)
     peaks = get_peaks(array)
     bin_peaks = array_to_binary(peaks)
     peaks_count = peaks.count
     return 0 if peaks_count == 0
     return 1 if peaks_count == 1
-    get_optimal_num(bin_peaks, array.size)
+    get_optimal_num(bin_peaks, array.size, peaks_count)
   end
 
   def get_peaks(array)
@@ -33,14 +34,36 @@ class Flags
     number
   end
 
-  def get_optimal_num(bin_peaks, array_size)
-    max_flags_count = (array_size ** ( 1.0 / 2)).floor
-    Array(2..max_flags_count).reverse.find do |mask|
-      mask_covers_the_array?(bin_peaks, mask)
+  def get_optimal_num(bin_peaks, array_size, peaks_count)
+    max_flags_count = [(array_size ** ( 1.0 / 2)).floor, peaks_count].min
+    optimal_number = 2
+    current_best = 2
+    Array(2..max_flags_count).reverse.each do |flags|
+      current_best = peakes_in_ranges_with_flag_limit(bin_peaks, flags)
+      optimal_number = current_best if current_best > optimal_number
+      return optimal_number if flags < optimal_number
     end
+    optimal_number
   end
 
-  def mask_covers_the_array?(bin_peaks, mask)
-    (0..mask.bit_length).all? { |shift| (bin_peaks & (mask << shift * mask.bit_length)).nonzero? }
+  def peakes_in_ranges(bin_num, range)
+    length = ( bin_num.to_s(2).size / range.to_f ).ceil
+    count = 0
+    mask = 2 ** range - 1
+    ( 0..length ).each do |num|
+      count += 1 if (bin_num & (mask << range * num)).nonzero?
+    end
+    count
+  end
+
+  def peakes_in_ranges_with_flag_limit(bin_num, range)
+    length = ( bin_num.to_s(2).size / range.to_f ).ceil
+    count = 0
+    mask = 2 ** range - 1
+    ( 0..length ).each do |num|
+      count += 1 if (bin_num & (mask << range * num)).nonzero?
+      return count if count == range
+    end
+    range
   end
 end
--- Flags/flags_test.rb
+++ Flags/flags_test.rb
@@ -5,4 +5,27 @@ describe Flags do
     array = [1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2]
     expect(subject.solution(array)).to eql(3)
   end
+
+  it "should find the number of flags that can be set on current trip with 3 peaks and big first gap" do
+    array = [0, 0, 0, 0, 0, 1, 0, 1, 0, 1]
+    expect(subject.solution(array)).to eql(2)
+  end
+
+  it "should get the number of ones in ranges" do
+    bin_array = "10000101000".to_i(2)
+    range = 2
+    expect( subject.peakes_in_ranges(bin_array, range) ).to eql(3)
+  end
+
+  it "should get the number of ones in ranges (2)" do
+    bin_array = "10100000".to_i(2)
+    range = 2
+    expect(subject.peakes_in_ranges(bin_array, range)).to eql(2)
+  end
+
+  it "should get peaks in ranges with flag limit" do
+    bin_array = "101000001010".to_i(2)
+    range = 2
+    expect(subject.peakes_in_ranges_with_flag_limit(bin_array, range)).to eql(2)
+  end
 end
